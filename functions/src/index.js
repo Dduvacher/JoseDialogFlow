1f26cf73921771d7193beb4efa50aa37ab8805ba
dduvacher <damien.duvacher@neo9.fr>
2018-09-24 00:41:45 +0200

    unitTest: Add first unit test

--- functions/src/index.js
+++ functions/src/index.js
@@ -7,70 +7,65 @@ const { WebhookClient } = require('dialogflow-fulfillment');
 const http = require('http');
 // const { Card, Suggestion } = require('dialogflow-fulfillment');
 
-const host = 'api.worldweatheronline.com';
-const wwoApiKey = '876d784e29d14d2782f115732182209';
+const host: string = 'api.worldweatheronline.com';
+const wwoApiKey: string = '876d784e29d14d2782f115732182209';
 
 process.env.DEBUG = 'dialogflow:debug';
+
+function callWeatherApi(city: string, date?: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    // Create the path for the HTTP request to get the weather
+    let path = `${'/premium/v1/weather.ashx?format=json&num_of_days=1&q='}${encodeURIComponent(city)}&key=${wwoApiKey}`;
+
+    if (date) {
+      path += `&date=${date}`;
+    }
+    console.log(`API Request: ${host}${path}`);
+
+    // Make the HTTP request to get the weather
+    http.get({ host, path }, (res) => {
+      let body = ''; // var to store the response chunks
+      res.on('data', (d) => { body += d; }); // store each response chunk
+      res.on('end', () => {
+        // After all the data has been received parse the JSON for desired data
+        const wwoResponse = JSON.parse(body);
+        const forecast = wwoResponse.data.weather[0];
+        const location = wwoResponse.data.request[0];
+
+        // Create response
+        const output = `Aujourd'hui à ${location.query} la température maximale est de ${forecast.maxtempC}°C et la température minimale est de ${forecast.mintempC}°C`;
+
+        // Resolve the promise with the output text
+        console.log(output);
+        resolve(output);
+      });
+      res.on('error', (error) => {
+        console.log(`Error calling the weather API: ${error}`);
+        reject();
+      });
+    });
+  });
+}
+
+async function giveWeather(agent): Promise<void> {
+  const { city }: {city: string} = agent.parameters;
+  const date: string = '';
+  // Call the weather API
+  await callWeatherApi(city, date).then((output): void => {
+    agent.add(output); // Return the results of the weather API to Dialogflow
+  }).catch(() => {
+    agent.add('Je ne sais pas quel temps il fait mais j\'espère qu\'il fait beau mais frais mais beau');
+  });
+}
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agentHook = new WebhookClient({ request, response });
   console.log(`Dialogflow Request headers: ${JSON.stringify(request.headers)}`);
   console.log(`Dialogflow Request body: ${JSON.stringify(request.body)}`);
 
-  function welcome(agent) {
-    agent.add('Welcome to my agent!');
-  }
-
-  function fallback(agent) {
-    agent.add('I didn\'t understand');
-    agent.add('I\'m sorry, can you try again?');
-  }
-
-  function callWeatherApi(city, date) {
-    return new Promise((resolve, reject) => {
-      // Create the path for the HTTP request to get the weather
-      const path = `${'/premium/v1/weather.ashx?format=json&num_of_days=1&q='}${encodeURIComponent(city)}&key=${wwoApiKey}&date=${date}`;
-      console.log(`API Request: ${host}${path}`);
-
-      // Make the HTTP request to get the weather
-      http.get({ host, path }, (res) => {
-        let body = ''; // var to store the response chunks
-        res.on('data', (d) => { body += d; }); // store each response chunk
-        res.on('end', () => {
-          // After all the data has been received parse the JSON for desired data
-          const wwoResponse = JSON.parse(body);
-          const forecast = wwoResponse.data.weather[0];
-          const location = wwoResponse.data.request[0];
-
-          // Create response
-          const output = `Aujourd'hui à ${location.query} la température maximale est de ${forecast.maxtempC}°C et la température minimale est de ${forecast.mintempC}°C`;
-
-          // Resolve the promise with the output text
-          console.log(output);
-          resolve(output);
-        });
-        res.on('error', (error) => {
-          console.log(`Error calling the weather API: ${error}`);
-          reject();
-        });
-      });
-    });
-  }
-
-  async function giveWeather(agent) {
-    const { city } = agent.parameters;
-    const date = '';
-    // Call the weather API
-    await callWeatherApi(city, date).then((output) => {
-      agent.add(output); // Return the results of the weather API to Dialogflow
-    }).catch(() => {
-      agent.add('Je ne sais pas quel temps il fait mais j\'espère qu\'il fait beau mais frais mais beau');
-    });
-  }
-
   const intentMap = new Map();
-  intentMap.set('Default Welcome Intent', welcome);
-  intentMap.set('Default Fallback Intent', fallback);
   intentMap.set('weather', giveWeather);
   agentHook.handleRequest(intentMap);
 });
+
+exports.callWeatherApi = callWeatherApi;
--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -8,42 +8,49 @@ describe('Cloud Functions', () => {
   });
 
   describe('dialogflowFirebaseFulfillment', () => {
-    it('should return a 200 code', (done) => {
-      const req = {
-        responseId: 'c35788dc-b7af-4fad-85b8-db73f527c245',
-        queryResult: {
-          queryText: 'Il fait moche à Nice ?',
-          parameters: {
-            city: 'Nice',
-          },
-          allRequiredParamsPresent: true,
-          fulfillmentMessages: [
-            {
-              text: {
-                text: [''],
-              },
-            },
-          ],
-          intent: {
-            name: 'projects/jose-15c01/agent/intents/4f1e8bd4-61e6-4c77-a5ab-bb4e8ef23620',
-            displayName: 'weather',
-          },
-          intentDetectionConfidence: 0.7,
-          languageCode: 'fr',
-        },
-        originalDetectIntentRequest: {
-          payload: {},
-        },
-        session: 'projects/jose-15c01/agent/sessions/e6fde53a-7b37-c238-a545-55b3e16f07bb',
-      };
-
-      const res = {
-        fullfillmentText: (text) => {
-          assert.equal(text, 'wesolu');
-          done();
-        },
-      };
-      myFunctions.dialogflowFirebaseFulfillment(req, res);
+    it('should return the right text result', async () => {
+      assert.equal(await myFunctions.callWeatherApi('Paris').then(output => output),
+        'Aujourd\'hui à Paris, France la température maximale est de 18°C et la température minimale est de 11°C');
     });
   });
+
+  // describe('dialogflowFirebaseFulfillment', () => {
+  //   it('should return a 200 code', (done) => {
+  //     const req = {
+  //       responseId: 'c35788dc-b7af-4fad-85b8-db73f527c245',
+  //       queryResult: {
+  //         queryText: 'Il fait moche à Nice ?',
+  //         parameters: {
+  //           city: 'Nice',
+  //         },
+  //         allRequiredParamsPresent: true,
+  //         fulfillmentMessages: [
+  //           {
+  //             text: {
+  //               text: [''],
+  //             },
+  //           },
+  //         ],
+  //         intent: {
+  //           name: 'projects/jose-15c01/agent/intents/4f1e8bd4-61e6-4c77-a5ab-bb4e8ef23620',
+  //           displayName: 'weather',
+  //         },
+  //         intentDetectionConfidence: 0.7,
+  //         languageCode: 'fr',
+  //       },
+  //       originalDetectIntentRequest: {
+  //         payload: {},
+  //       },
+  //       session: 'projects/jose-15c01/agent/sessions/e6fde53a-7b37-c238-a545-55b3e16f07bb',
+  //     };
+  //
+  //     const res = {
+  //       fullfillmentText: (text) => {
+  //         assert.equal(text, 'wesolu');
+  //         done();
+  //       },
+  //     };
+  //     myFunctions.dialogflowFirebaseFulfillment(req, res);
+  //   });
+  // });
 });
